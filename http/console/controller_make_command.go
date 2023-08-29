9800c0a451b3f77d446d3bdb94e01386e083eca7
林森 <linsen@limayao.com>
2023-08-29 18:26:00 +0800

    fix: 修改基础目录结构
    Signed-off-by: 林森 <linsen@limayao.com>

--- http/console/controller_make_command.go
+++ http/console/controller_make_command.go
@@ -87,7 +87,7 @@ func (receiver *ControllerMakeCommand) getPath(name string) string {
 
 	controllerName, _, folderPath := receiver.parseName(name)
 
-	return filepath.Join(pwd, support.DirApp, support.DirController, folderPath, str.Camel2Case(controllerName)+".go")
+	return filepath.Join(pwd, support.DirController, folderPath, str.Camel2Case(controllerName)+".go")
 }
 
 // parseName Parse the name to get the controller name, package name and folder path.
